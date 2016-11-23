236fb164d7f2981aac54cfe6c7960e9d11a04272
David Oliveira <david.oliveira@skyscanner.net>
2016-11-23 19:57:29 +0000

    WIP

--- test.js
+++ test.js
@@ -1,15 +1,16 @@
 var
     AWS = require('./lib/'),
-    aws = new AWS({regions: ['eu-west-1', 'eu-central-1']});
+    aws = new AWS({regions: ['eu-west-1', 'eu-central-1'], cache:{ec2: Infinity, elb:300}});
 
 
 
-aws.getResources(aws.$("!asg elb#Test"), function(err, res){
+aws.getResources(aws.$('ec2'), function(err, res){
     if ( err ) {
         console.log("ERR: ", err);
         return process.exit(-1);
     }
 //    console.log(JSON.stringify(res,0,4));
+	console.log("Result:");
     res.forEach(function(r){
         console.log(r._id);
     });
