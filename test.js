493881d49b2a529feb8388e69827b46756be7c4b
David Oliveira <david@Davids-MacBook.local>
2016-12-01 20:56:27 +0000

    WIP

--- test.js
+++ test.js
@@ -1,53 +1,20 @@
 var
     AWS = require('./lib/'),
-    aws = new AWS({regions: ['eu-west-1', 'eu-central-1'], cache:{ec2: Infinity, elb:300, asg: Infinity}, cacheDriver: "dynamo"}),
+    aws = new AWS({regions: ['eu-west-1', 'eu-central-1'], cache:{ec2: Infinity, elb:300, asg: Infinity}}),
     start = new Date();
 
 
 
-aws.getResources(aws.$('ec2'), function(err, res){
+aws.ec2.autoscaling.scale(aws.$('asg[Project=Test]'), '+1', function(err, res){
     if ( err ) {
         console.log("ERR: ", err);
         return process.exit(-1);
     }
-//    console.log(JSON.stringify(res,0,4));
-	console.log("Result ("+res.length+"):");
-    res.forEach(function(r){
+    console.log(res);
+/*    res.forEach(function(r){
         console.log(r._id);
     });
+*/
+    console.log(res);
     console.log("Took: ", (new Date()-start), "ms");
-
-    console.log("--");
-    start = new Date();
-    aws.getResources(aws.$('!asg[TagOne^=Value] ec2'), function(err, res){
-        if ( err ) {
-            console.log("ERR: ", err);
-            return process.exit(-1);
-        }
-
-        console.log("Result ("+res.length+"):");
-        res.forEach(function(r){
-            console.log(r._id);
-        });
-        console.log("Took: ", (new Date()-start), "ms");
-
-        console.log("--");
-        start = new Date();
-        aws.getResources(aws.$('!asg[TagOne^=Value] ec2'), function(err, res){
-            if ( err ) {
-                console.log("ERR: ", err);
-                return process.exit(-1);
-            }
-
-            console.log("Result ("+res.length+"):");
-            res.forEach(function(r){
-                console.log(r._id);
-            });
-            console.log("Took: ", (new Date()-start), "ms");
-
-            return process.exit(0);
-        });
-
-
-    });
 });
